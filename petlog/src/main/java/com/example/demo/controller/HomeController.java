4856cc0e98b7384fbb75d8b97ddfd899857eec36
unknown <vnfdlvxodus@naver.com>
2025-03-29 17:41:55 +0900

    [ADD] 프로젝트 기본 세팅

---
+++ petlog/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/home")
+	public void home(HttpServletResponse httpServletResponse){
+		System.out.println("HOME");
+	}
+}
