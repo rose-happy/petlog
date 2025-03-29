64771a0c3e51d8f56bee18fd0d5054571b4bdbcf
rose-happy <vnfdlvxodus@naver.com>
2025-03-29 19:03:41 +0900

    mihee

--- petlog/src/main/java/com/example/demo/controller/HomeController.java
+++ petlog/src/main/java/com/example/demo/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/home")
 	public void home(HttpServletResponse httpServletResponse){
-		System.out.println("HOM22E33S2");
+		System.out.println("HELLO");
 	}
 }
